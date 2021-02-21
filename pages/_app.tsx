65a53c407d93a6c5c0696a3872b6065b0d5fc6bc
Shehab Tweel <shehabtweel@gmail.com>
2021-02-21 11:52:03 +0200

    Put bootstrap in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import MainLayout from '../layouts/main';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <MainLayout>
+    <Component {...pageProps} />
+  </MainLayout>
 }
 
 export default MyApp;
