4aeb684d4784521f3d73b7f7de8ccef79cc60c17
Shehab Tweel <shehabtweel@gmail.com>
2021-02-21 15:08:16 +0200

    ..

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 export default function MainLayout({ children }) {
     return (
         <>
             <Header />
-            {children}
+            <Container>
+                {children}
+            </Container>
+            <Footer />
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
-import Head from 'next/head';
+// import Head from 'next/head';
+import Link from 'next/link';
+
+import { Button, Jumbotron } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <>Hello</>
+    <>
+      <Jumbotron>
+        <h1>Shehab Tweel</h1>
+        An 11 year old programmer, born from Libya.
+        <br/>
+        <Link href="/about">
+          <Button>Learn more about me</Button>
+        </Link>
+      </Jumbotron>
+    </>
   )
 }
